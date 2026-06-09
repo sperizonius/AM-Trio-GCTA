f8095c4cc79230c28bfa2ed10b31e7532d311993
sperizonius <129739213+sperizonius@users.noreply.github.com>
2026-06-09 16:24:05 +0200

    Update empirical_make_phen_files.py

--- code/empirical application/gcta/empirical_make_phen_files.py
+++ code/empirical application/gcta/empirical_make_phen_files.py
@@ -1,23 +1,30 @@
-## Script to make reorder files that only contain the IDs of the children to be used with PLINK to make PLINK files for the children
+## Script to make .phen files containing the phenotypes of the children for the GCTA analysis
 ## Sofie Perizonius - 9-6-2026
 
-# make children subset files
-bl_reorder = pd.read_csv("for_trio_gcta/reorder_birth_length.txt", sep=r"\s+", header=None)
-n_bl = int(bl_reorder.shape[0] /3)
-bl_reorder_children = bl_reorder.iloc[2*n_bl : 3*n_bl + 1, :]
-bl_reorder_children.to_csv('for_trio_gcta/birth_length_children.txt', sep="\t", header=False, index=False)
+import numpy as np
+import pandas as pd
 
-bw_reorder = pd.read_csv("for_trio_gcta/reorder_birth_weight.txt", sep=r"\s+", header=None)
-n_bw = int(bw_reorder.shape[0] /3)
-bw_reorder_children = bw_reorder.iloc[2*n_bw : 3*n_bw + 1, :]
-bw_reorder_children.to_csv('for_trio_gcta/birth_weight_children.txt', sep="\t", header=False, index=False)
+def make_phen_children(fam_path, phenotype_path):    
+    # read fam file 
+    fam = pd.read_csv(fam_path, sep=r"\s+", header=None)
+    fam = pd.DataFrame(fam)
 
-height_reorder = pd.read_csv("for_trio_gcta/reorder_height.txt", sep=r"\s+", header=None)
-n_height = int(height_reorder.shape[0] /3)
-height_reorder_children = height_reorder.iloc[2*n_height : 3*n_height + 1, :]
-height_reorder_children.to_csv('for_trio_gcta/height_children.txt', sep="\t", header=False, index=False)
+    phe_csv = pd.read_csv(phenotype_path)
+    n_trait = int(phe_csv.shape[0] / 3)
+    phe_column =  phe_csv.iloc[2*n_trait : 3*n_trait + 1, 3]
+    phe = fam.loc[:, [0,1]]
+    phe[2] = phe_column.values    
 
-edu_reorder = pd.read_csv("for_trio_gcta/reorder_edu.txt", sep=r"\s+", header=None)
-n_edu = int(edu_reorder.shape[0] /3)
-edu_reorder_children = edu_reorder.iloc[2*n_edu : 3*n_edu + 1, :]
-edu_reorder_children.to_csv('for_trio_gcta/edu_children.txt', sep="\t", header=False, index=False)
+    return phe
+
+phen_bw = make_phen_children('for_gcta/birth_weight_children.fam', "for_trio_gcta/birth_weight_trios.csv")
+phen_bw.to_csv('for_gcta/birth_weight_children.phe', sep="\t", header=False, index=False)
+
+phen_bl = make_phen_children('for_gcta/birth_length_children.fam', "for_trio_gcta/birth_length_trios.csv")
+phen_bl.to_csv('for_gcta/birth_length_children.phe', sep="\t", header=False, index=False)
+
+phen_height = make_phen_children('for_gcta/height_children.fam', "for_trio_gcta/height_trios.csv")
+phen_height.to_csv('for_gcta/height_children.phe', sep="\t", header=False, index=False)
+
+phen_edu = make_phen_children('for_gcta/edu_children.fam', "for_trio_gcta/edu_trios.csv")
+phen_edu.to_csv('for_gcta/edu_children.phe', sep="\t", header=False, index=False)
