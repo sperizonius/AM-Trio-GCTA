c2b4032c247765c4285a55486d9057fc85a4f15e
sperizonius <129739213+sperizonius@users.noreply.github.com>
2026-06-09 11:16:14 +0200

    Update make_reorder_subsets.py

--- code/simulation scripts/gcta/make_reorder_subsets.py
+++ code/simulation scripts/gcta/make_reorder_subsets.py
@@ -1,7 +1,6 @@
 ## Script used to obtain subset of the original reorder file (used for reordering simulated datasets according to required Trio-GCTA 
 ## required order) that only contains IDs of the children for the subsequent GCTA analysis
 
-
 import numpy as np
 import pandas as pd
 
